5fa6066c9c0d077a798b30d3ac5f8abda33b9e05
nohum <nohum@noreply.com>
2014-03-01 22:12:12 +0100

    changed config files

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!file_exists(__DIR__.'/../resources/config/prod.php')) {
+	exit(0);
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+require __DIR__.'/../resources/config/prod.php';
+require __DIR__.'/../src/app.php';
+
+require __DIR__.'/../src/controllers.php';
+
+$app['http_cache']->run();
---
+++ web/index_dev.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!file_exists(__DIR__.'/../resources/config/dev.php')) {
+	exit(0);
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+Symfony\Component\Debug\Debug::enable();
+
+$app = new Silex\Application();
+
+require __DIR__.'/../resources/config/dev.php';
+require __DIR__.'/../src/app.php';
+
+require __DIR__.'/../src/controllers.php';
+
+$app->run();
