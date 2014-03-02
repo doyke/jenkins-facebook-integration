2bec7bde7eb5fab7a99468fad609670b93c43498
nohum <nohum@noreply.com>
2014-03-02 21:13:59 +0100

    changed deployment script and console to work nicely togheter

--- src/SKE/Composer/Script.php
+++ src/SKE/Composer/Script.php
@@ -6,10 +6,32 @@ class Script
 {
     public static function install()
     {
-        chmod('resources/cache', 0777);
-        chmod('resources/log', 0777);
-        chmod('web/assets', 0777);
+        self::chmodOrMkdir('resources/cache', 0777);
+	    self::chmodOrMkdir('resources/log', 0777);
+	    self::chmodOrMkdir('web/assets', 0777);
         chmod('console', 0500);
+
+	    self::checkProdFile();
+
         exec('php console assetic:dump');
     }
+
+	private static function chmodOrMkdir($directory, $mode)
+	{
+		if (!is_dir($directory))
+		{
+			mkdir($directory, $mode, true);
+			return;
+		}
+
+		chmod($directory, $mode);
+	}
+
+	private static function checkProdFile()
+	{
+		if (!file_exists('resources/config/prod.php'))
+		{
+			copy('resources/config/prod.php.dist', 'resources/config/prod.php');
+		}
+	}
 }
