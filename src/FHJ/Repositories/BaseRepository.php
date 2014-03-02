6a376440f362aad4c8e22913c7fbb3c6995d556a
nohum <nohum@noreply.com>
2014-03-02 21:16:26 +0100

    changed locale filename in app.php; added BaseRepository class

---
+++ src/FHJ/Repositories/BaseRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace FHJ\Repositories;
+
+use Doctrine\DBAL\Connection;
+use Monolog\Logger;
+
+class BaseRepository {
+
+	/**
+	 * @var Connection
+	 */
+	private $connection;
+
+	/**
+	 * @var Logger
+	 */
+	private $logger;
+
+	public function __construct(Connection $connection, Logger $logger) {
+		$this->connection = $connection;
+		$this->logger = $logger;
+	}
+
+	/**
+	 * @return \Doctrine\DBAL\Connection
+	 */
+	protected function getConnection()
+	{
+		return $this->connection;
+	}
+
+	/**
+	 * @return \Monolog\Logger
+	 */
+	protected function getLogger()
+	{
+		return $this->logger;
+	}
+
+}
