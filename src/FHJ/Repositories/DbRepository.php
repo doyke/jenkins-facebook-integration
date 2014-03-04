be03c23a7a5a771ddd0e0d1287ce29382ec514e3
Wolfgang Gaar <nohum@users.noreply.github.com>
2014-03-04 09:43:29 -0500

    added database repository files

---
+++ src/FHJ/Repositories/DbRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FHJ\Repositories;
+
+class DbRepository extends BaseRepository {
+
+
+
+}
---
+++ src/FHJ/Repositories/SVNPlotRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FHJ\Repositories;
+
+class SVNPlotRepository extends BaseRepository {
+
+
+
+}
