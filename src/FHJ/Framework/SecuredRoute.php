850166a5abebbf8598182a852d49c34b6deb2938
nohum <eiskalterengel@truthfactory.tk>
2014-03-17 19:32:38 +0100

    SecuredRoute: fixed missing use-statement; re-added use-statement for SecurityTrait

--- src/FHJ/Framework/SecuredRoute.php
+++ src/FHJ/Framework/SecuredRoute.php
@@ -5,7 +5,9 @@ namespace FHJ\Framework;
 
 use Silex\Application;
 use Silex\Route;
+use Silex\Route\SecurityTrait;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use FHJ\Entities\User;
 
 /**
@@ -15,17 +17,8 @@ use FHJ\Entities\User;
 class SecuredRoute extends Route {
 
 	// this provides us the secure() method for restricting a controller method to certain user roles
-	//use SecurityTrait;
-	
-	// copied from Silex\Route\SecurityTrait to provide compatibility for PHP 5.3
-	public function secure($roles) {
-        $this->before(function ($request, $app) use ($roles) {
-            if (!$app['security']->isGranted($roles)) {
-                throw new AccessDeniedException();
-            }
-        });
-    }
-    
+	use SecurityTrait;
+
     public function onlyIfProjectsAllowed() {
         $this->before(function (Application $app) {
             $user = $app['security']->getUser();
@@ -52,14 +45,13 @@ class SecuredRoute extends Route {
             if ($user === null || !$user instanceof User) {
                 throw new AccessDeniedException();
             }
-            
-            $projectId = $request->attributes->get('project');
-            
+
             if ($app['security']->isGranted(array('ROLE_ADMIN'))) {
                 return;
             }
-            
-            $project = $app['repository.projects']->findProjectById(intval($projectId));
+
+	        $projectId = $request->attributes->get('project');
+	        $project = $app['repository.projects']->findProjectById(intval($projectId));
             if ($project === null || $project->getUserId() === $user->getId()) {
                 return;
             }
